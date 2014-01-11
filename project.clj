e84f50c47373753d3c94507cf68fbe65ecc67204
Alexander Vykhodtsev <avflance@gmail.com>
2014-01-12 00:04:34 +0700

    SyncItems

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"] [necessary-evil "2.0.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"] 
+                 [necessary-evil "2.0.0"] 
+                 [digest "1.4.3"]]
   :main clj.core/-main)
--- src/clj/core.clj
+++ src/clj/core.clj
@@ -1,14 +1,46 @@
 (ns clj.core)
 
 (require '[necessary-evil.core :as xml-rpc])
+(require 'digest)
+
+(def password)
+(def username)
 
 (defn xmlrpc [method-name args]
-  (xml-rpc/call* "http://www.livejournal.com/interface/xmlrpc" method-name args 
+  (xml-rpc/call* "http://www.livejournal.com/interface/xmlrpc" method-name [args]
                   :request { :headers { "User-Agent" "CLJ v0.0.1 <avflance@gmail.com>" }}))
-  
-(defn auth []
-  (xmlrpc :LJ.XMLRPC.getchallenge []))
+
+(defn get-challenge []
+  (:challenge (xmlrpc :LJ.XMLRPC.getchallenge [])))
+
+(defn digest [challenge]
+  (digest/md5 (str challenge (digest/md5 password))))
+
+(defn auth-params [challenge digest]
+  {:username username
+   :auth_method "challenge"
+   :auth_challenge challenge
+   :auth_response digest
+   :ver 1})
+
+(defn exec [method args]
+  (let [challenge (get-challenge)]
+    (xmlrpc method
+      (merge args (auth-params challenge (digest challenge))))))
+
+(defn sync-items [last-sync]
+  (exec "LJ.XMLRPC.syncitems"
+    (if last-sync
+        {:lastsync last-sync}
+        nil)))
+
+(defn run []
+  (sync-items "2010-01-01 00:00:00"))
 
 (defn -main
-  []
-  (println (auth)))
+  [username password]
+  (def username username)
+  (def password password)
+  (println (run)))
+
+(run)
