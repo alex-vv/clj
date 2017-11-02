93729988897d2531c72434aa7e56c702ba50a815
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2017-11-02 10:08:01 +0600

    Add a basic unit test

--- test/clj/core_test.clj
+++ test/clj/core_test.clj
@@ -1,7 +1,18 @@
 (ns clj.core-test
+  (:use [clojure.data.xml :only [indent-str emit-str]]
+        [clj.utils])
   (:require [clojure.test :refer :all]
-            [clj.core :refer :all]))
+            [clj.core :refer :all]
+            [clojure.edn]))
+
+(defn fixture-string [name]
+  (slurp (str "test/clj/fixtures/" name)))
+
+(defn fixture-edn [name]
+  (clojure.edn/read-string (fixture-string name)))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "post as xml"
+    (let [post (fixture-edn "post.edn")
+          post-xml (fixture-string "post.xml")]
+      (is (= post-xml (indent-str (post-as-xml post)))))))
