d84d1161db586f64a40d3e75c261631e0be52eb6
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2017-12-09 21:49:39 +0600

    Update to clojure 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/alex-vv/clj"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [necessary-evil "2.0.2"]
                  [digest "1.4.6"]
                  [clj-time "0.14.0"],
